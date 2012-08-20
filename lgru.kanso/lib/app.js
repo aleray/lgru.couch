a4a5b03495fa62ffea672f8848733a38997ef863
Alexandre Leray <alexandre@alexandreleray.com>
2012-08-20 16:04:30 +0200

    Continued first my exploration of kanso.
    I have created a couple of new views, lists and shows, and I'm
    experimenting with the misc packages available for kanso.

--- lgru.kanso/lib/app.js
+++ lgru.kanso/lib/app.js
@@ -18,18 +18,73 @@ exports.views = {
 
 exports.lists = {
     publications: function(head, req) {
-        var _ = require('underscore')._;
-        log(_);
+        var _ = require('underscore')._,
+            Handlebars = require('handlebars');
+
+        Handlebars.registerHelper('join', function(items) {
+            return items.join(', ');
+        });
 
         provides("html", function() {
-            //var rows = [];
+            var rows = [];
 
-            //while (row = getRow()) {
-                //rows.push(row.value);
-            //}
+            while (row = getRow()) {
+                rows.push(row.value);
+            }
 
-            //var byYear = _.groupBy(rows.map(function(r) {return r.value;}), 'year');
-            return 'ok';
+            var byYear = _.groupBy(rows, function (row) { return row.bibtex.year });
+
+            _.each(byYear, function(publications, year) { 
+                send('<h1>' + year + '</h1>');
+                _.each(publications, function(publication) { 
+                    var bib = publication.bibtex;
+                    send(Handlebars.templates['partials/publication-row.html'](publication));
+                });
+            });
         });
     },
+};
+
+exports.shows = {
+    index: function(doc, req) {
+        var showdown = require('showdown'),
+            sd = new showdown.converter();
+
+        provides("html", function() {
+            send(sd.makeHtml(doc.body));
+        });
+    },
+    add: function(doc, req) {
+        provides("html", function() {
+            send("here comes the form to add publications");
+        });
+    },
+    reviews: function(doc, req) {
+        provides("html", function() {
+            send("here comes the form the review");
+        });
+    },
+    publications: function(doc, req) {
+        var showdown = require('showdown'),
+            sd = new showdown.converter(),
+            Handlebars = require('handlebars');
+
+        provides("html", function() {
+            send(Handlebars.templates['base.html']({
+                content: sd.makeHtml(doc.body)
+            }));
+        });
+    },
+    bibtex: function(doc, req) {
+        var _ = require('underscore')._,
+            Handlebars = require('handlebars');
+
+        var ctx = {"type": doc.bibtex_type};
+        ctx = _.extend(ctx, doc.bibtex);
+
+        return {
+           "headers" : {"Content-Type" : "text/plain"},
+           "body" : Handlebars.templates['publication.bib'](ctx)
+        }
+    }
 };
---
+++ lgru.kanso/lib/validate.js
@@ -0,0 +1,7 @@
+var couchtypes = require('couchtypes/types'),
+    types = require('./types');
+
+
+module.exports = function (newDoc, oldDoc, userCtx) {
+    couchtypes.validate_doc_update(types, newDoc, oldDoc, userCtx);
+};
